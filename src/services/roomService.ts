ee64755fe357b1b81feedce4502caee76e2da586
Carlos Alejandro <carmartinez411@gmail.com>
2025-05-09 14:01:21 -0500

    Corrijo carga dinámica del JSON

--- src/services/roomService.ts
+++ src/services/roomService.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Room } from "../interfaces/Room";
 
-const API_URL = "/src/data/rooms.json";
+const API_URL = "/data/rooms.json";
 
 export const getPopularRooms = async (): Promise<Room[]> => {
   try {
